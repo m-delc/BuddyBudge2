16b4c121c0977bd33f61cbd37e2ec2f14789681a
Michael Delcambre <delcambrem@gmail.com>
2022-08-10 21:10:07 -0500

    default url was wrong, changed it to new correct one, also reverted procfile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
 
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.default_options = {from: Rails.application.credentials.dig(:google_smtp, :email)}
   config.action_mailer.delivery_method = :smtp
